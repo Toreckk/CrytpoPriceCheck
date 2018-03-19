2d200e104b16e1fa7ee42e14e5a35eea84c70b43
Juan Coret <coret.juan@gmail.com>
2018-03-19 15:39:05 +0100

    - Base UI created (will change in the future) - Gets all the infro for the top 50 coins from coinmarketcap - You can select a coin and it will update the tab with the name of the currency and price

---
+++ CryptoPriceCheck/CryptoPriceCheck/Crypto/Coin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CryptoPriceChecker.Crypto
+{
+    public class Coin
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string symbol { get; set; }
+        public string rank { get; set; }
+
+        public string price_usd { get; set; }
+        public string price_btc { get; set; }
+
+        [JsonProperty(PropertyName = "24h_volume_usd")]
+        public string volume_usd_24h { get; set; }//Cant start with a number
+
+        public string market_cap_usd { get; set; }
+        public string available_supply { get; set; }
+        public string total_supply { get; set; }
+        public string percent_change_1h { get; set; }
+        public string percent_change_24h { get; set; }
+        public string percent_change_7d { get; set; }
+        public string last_updated { get; set; }
+    }
+}
---
+++ CryptoPriceCheck/CryptoPriceCheck/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CryptoPriceChecker.Crypto;
+using Newtonsoft.Json;
+
+namespace CryptoPriceCheck
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Coin> Cryptos { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GetCryptoCurrencyInfo();
+
+            DataContext = this;
+        }
+
+        public void GetCryptoCurrencyInfo()
+        {
+            HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create(string.Format("https://api.coinmarketcap.com/v1/ticker/?limit=50"));
+
+            WebReq.Method = "GET";
+
+            HttpWebResponse WebResp = (HttpWebResponse)WebReq.GetResponse();
+
+            Console.WriteLine(WebResp.StatusCode);
+            Console.WriteLine(WebResp.Server);
+
+            string jsonString;
+            using (Stream stream = WebResp.GetResponseStream())   //modified from your code since the using statement disposes the stream automatically when done
+            {
+                StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
+                jsonString = reader.ReadToEnd();
+            }
+
+            Cryptos = JsonConvert.DeserializeObject<List<Coin>>(jsonString);
+        }
+
+        //ComboBox Changes
+
+        private void cb1_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            int rank = cb1.SelectedIndex;
+            tb1.Header = $"{Cryptos[rank].name} {Cryptos[rank].price_usd}$";
+        }
+
+        private void cb2_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            int rank = cb2.SelectedIndex;
+            tb2.Header = $"{Cryptos[rank].name} {Cryptos[rank].price_usd}$";
+        }
+
+        private void cb3_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            int rank = cb3.SelectedIndex;
+            tb3.Header = $"{Cryptos[rank].name} {Cryptos[rank].price_usd}$";
+        }
+    }
+}
